6ab872ef38078afc6e68d72975af63334da371e5
Angra Santos <angra.santos@hotmail.com.br>
2022-05-03 02:01:52 -0300

    Initial commit

---
+++ src/programa/OperadoresLogicos.java
@@ -0,0 +1,31 @@
+package programa;
+
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+    //Trabalhou na terça = (V ou F)
+    //Trabalhou na quinta = (V ou F)
+
+    boolean trabalho1 = true;
+    boolean trabalho2 = true;
+
+
+    //&& = Este símbolo é um operador lógico relacional, que concatena uma condição e a outra condição (joao E maria)
+    boolean comprouTV50 = trabalho1 && trabalho2;
+    System.out.println("Comprou TV 50\" ? " + comprouTV50);
+
+    // || = Este símbolo é um operador lógico relacional, que impõe a duas condições (joao OU maria)
+    boolean comprouTV32 = trabalho1 || trabalho2;
+    System.out.println("Comprou TV 32\" ? " + comprouTV32);
+
+    // ! = Objeto relacional unário de negação
+    // Utilização de 2 operadores lógicos relacionais distintos (Unário e Binário)
+    boolean comprouSorvete = !trabalho1 && !trabalho2;
+        System.out.println("Comprou sorvete ? " + comprouSorvete);
+
+    boolean maisSaudavel = !comprouSorvete;
+        System.out.println("Mais saudavel ? " + maisSaudavel);
+
+
+    }
+}
