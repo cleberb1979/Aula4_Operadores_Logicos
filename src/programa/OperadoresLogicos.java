4145047020e6a6897e0d286232459eddaf6e35ac
cleberb1979 <luiz.cleber@gmail.com>
2023-06-04 14:32:27 -0300

    updates

--- src/programa/OperadoresLogicos.java
+++ src/programa/OperadoresLogicos.java
@@ -12,19 +12,19 @@ public class OperadoresLogicos {
 
     //&& = Este símbolo é um operador lógico relacional, que concatena uma condição e a outra condição (joao E maria)
     boolean comprouTV50 = trabalho1 && trabalho2;
-    System.out.println("Comprou TV 50\" ? " + comprouTV50);
+    System.out.println("Comprou TV 50\"? " + comprouTV50);
 
     // || = Este símbolo é um operador lógico relacional, que impõe a duas condições (joao OU maria)
     boolean comprouTV32 = trabalho1 || trabalho2;
-    System.out.println("Comprou TV 32\" ? " + comprouTV32);
+    System.out.println("Comprou TV 32\"? " + comprouTV32);
 
     // ! = Objeto relacional unário de negação
     // Utilização de 2 operadores lógicos relacionais distintos (Unário e Binário)
     boolean comprouSorvete = !trabalho1 && !trabalho2;
-        System.out.println("Comprou sorvete ? " + comprouSorvete);
+        System.out.println("Comprou sorvete? " + comprouSorvete);
 
     boolean maisSaudavel = !comprouSorvete;
-        System.out.println("Mais saudavel ? " + maisSaudavel);
+        System.out.println("Mais saudavel? " + maisSaudavel);
 
 
     }
